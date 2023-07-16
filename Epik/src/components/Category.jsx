9f66018fa116f46732c269c817511a1ef21fd419
Zahra Tabatabaee <Zahra_Tabatabaee@comp.iust.ac.ir>
2023-07-16 14:47:24 +0330

    clean category code

--- Epik/src/components/Category.jsx
+++ Epik/src/components/Category.jsx
@@ -3,9 +3,7 @@ import Card from "./Card.jsx";
 import gameimage from '../assets/images/gameimage.jpg';
 import '../assets/styles/Category.scss'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-
-function Category() {
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';function Category() {
   const [firstGameIndex, setFirstGameIndex] = useState(0);
   
   const games = [
@@ -47,47 +45,29 @@ function Category() {
     }
   ];        
 
-  // Define the number of games to show per row for different screen sizes
-  const gamesPerRow = {
-    small: 1,
-    medium: 2,
-    large: 3
-  };
-
-  // Define the number of games to show for different screen sizes
-  const gamesToShow = {
-    small: 3,
-    medium: 4,
-    large: 5
-  };
-
-  // Get the current screen size
-  const screenSize = window.innerWidth < 768 ? "small" : window.innerWidth < 992 ? "medium" : "large";
-
   const handleNextClick = () => {
-    setFirstGameIndex((prevIndex) => Math.min(prevIndex + gamesToShow[screenSize], games.length - gamesToShow[screenSize]));
+    setFirstGameIndex((prevIndex) => Math.min(prevIndex + 5, games.length - 5));
     document.querySelector(".Category-cards").classList.add("slide-left");
   };
 
   const handlePrevClick = () => {
-    setFirstGameIndex((prevIndex) => Math.max(prevIndex - gamesToShow[screenSize], 0));
+    setFirstGameIndex((prevIndex) => Math.max(prevIndex - 5, 0));
     document.querySelector(".Category-cards").classList.add("slide-right");
   };
 
-  // Get the games to display based on the current screen size
-  const displayedGames = games.slice(firstGameIndex, firstGameIndex + gamesToShow[screenSize]);
+  const displayedGames = games.slice(firstGameIndex, firstGameIndex + 5);
 
   return (
     <div className="Category">
       <div className="Category-title">
         <h1>Category</h1>
-        <div className="Category-buttons">
-          <ArrowBackIosNewIcon onClick={handlePrevClick} disabled={firstGameIndex === 0}/>
-          <ArrowForwardIosIcon onClick={handleNextClick} disabled={firstGameIndex + gamesToShow[screenSize] >= games.length} />
-        </div>
+              <div className="Category-buttons">
+        <ArrowBackIosNewIcon onClick={handlePrevClick} disabled={firstGameIndex === 0}/>
+        <ArrowForwardIosIcon onClick={handleNextClick} disabled={firstGameIndex + 5 >= games.length} />
+      </div>
       </div>
 
-      <div className={`Category-cards cards-${gamesPerRow[screenSize]}`}>
+      <div className="Category-cards">
         {displayedGames.map((game) => (
           <Card key={game.id} Title={game.title} Cost={game.cost} Img={game.image} />
         ))}
