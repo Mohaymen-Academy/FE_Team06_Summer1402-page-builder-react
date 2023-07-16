ea0e56ef1ca72442cb3c22bb0b847bc446d0fc46
hesam <bhesam2000@yahoo.com>
2023-07-16 12:17:53 +0430

    header complete 90%

--- Epik/src/components/Header.jsx
+++ Epik/src/components/Header.jsx
@@ -3,10 +3,59 @@ import epicgames from '../assets/images/epicgames.jpeg';
 import header from '../assets/styles/header.css'
 const Header = () => {
     return (
-        <div 
-        className='header'>
-            header
-            <img src={epicgames} alt="" />             
+        <div
+            className={'header'}>
+            <div
+                className='LeftHeader'>
+                <a href="#">
+                    <img className={'logo'} src={epicgames} alt="" />
+                </a>
+                <div
+                    style={{
+                        display: 'flex'
+                        , paddingLeft: '10px', paddingRight: '10px'}}>
+                    <a href="#" >
+                        <p
+                            className={'headerText'}>
+                            Store
+                        </p>
+                    </a>
+                    <a href="">
+
+                        <p
+                            className={'headerText'}>
+                            Distributation
+                        </p>
+                    </a>
+                    <a href="">
+                        <p
+                            className={'headerText'}>
+                            Support
+                        </p>
+                    </a>
+                </div>
+                <div
+                style={{display:'flex'}}>
+                        <div
+                        className='vl'>
+                        </div>
+                        <div
+                        style={{paddingLeft:"10px"}}>
+                            <a href="">
+                            <p>
+                                Unreal Engine
+                            </p>
+                            </a>
+                        </div>
+                </div>
+            </div>
+            <div
+                className={'RightHeader'}>
+                {/* TODO */}
+                {/* <img src="" alt="" /> */}
+                <button>Sign In</button>
+                <button>Download</button>
+            </div>
         </div>
     );
 }
