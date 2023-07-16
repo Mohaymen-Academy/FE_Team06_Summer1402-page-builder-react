aab818209b411efb243cfcf41fb4e3ae9ee83127
Zahra Tabatabaee <Zahra_Tabatabaee@comp.iust.ac.ir>
2023-07-16 15:26:24 +0330

    add cardcontainer

--- Epik/src/Router.jsx
+++ Epik/src/Router.jsx
@@ -4,6 +4,8 @@ import Header from "./components/Header.jsx";
 import HomeSlider from "./components/HomeSlider.jsx";
 import Footer from './components/Footer.jsx';
 import Category from "./components/Category.jsx";
+import CardContainer from "./components/CardContainer.jsx";
+
 
 import React, { usestate , useEffect } from "react";
 
@@ -16,6 +18,7 @@ const AppRouter = () => {
           <Route path='/HomeSlider' exact element={<HomeSlider/>} />
           <Route path='/Footer' exact element={<Footer/>} />
           <Route path='/Category' exact element={<Category/>} />
+          <Route path='/CardContainer' exact element={<CardContainer/>} />
         </Routes>
       </BrowserRouter>
   );
---
+++ Epik/src/components/CardContainer.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import HomeCard from "./HomeCard.jsx";
+import gameimage from '../assets/images/gameimage.jpg';
+import '../assets/styles/Category.scss'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+
+function CardContainer() {
+  const games = [
+    {
+      id: 1,
+      title: "Game Title 1",
+      cost: "1.99$",
+      image: gameimage
+    },
+    {
+      id: 2,
+      title: "Game Title 2",
+      cost: "1.99$",
+      image: gameimage
+    },
+    {
+      id: 3,
+      title: "Game Title 3",
+      cost: "1.99$",
+      image: gameimage
+    },
+    {
+      id: 4,
+      title: "Game Title 4",
+      cost: "1.99$",
+      image: gameimage
+    },
+    {
+      id: 5,
+      title: "Game Title 5",
+      cost: "1.99$",
+      image: gameimage
+    },
+    {
+      id: 6,
+      title: "Game Title 6",
+      cost: "1.99$",
+      image: gameimage
+    }
+  ];        
+
+  return (
+      <div className="Category-cards">
+        {games.map((game) => (
+          <HomeCard key={game.id} Title={game.title}  Img={game.image} />
+        ))}
+      </div>
+  );
+}
+
+export default CardContainer;
