acea0e78c748406f67cdb86561f87ae871dbc09c
hesam <bhesam2000@yahoo.com>
2023-07-17 10:35:57 +0430

    footer complete

--- Epik/src/components/Footer.jsx
+++ Epik/src/components/Footer.jsx
@@ -3,11 +3,15 @@ import '../assets/styles/footer.css'
 export default function Footer() {
     return (
         <div
-        className='footer'>
+            className='footer'>
             <div className='Icons'></div>
             <div
                 className='tablecontent'>
-                <table>
+                <table
+                    style={{
+                        // textAlign:'left'
+                    }}
+                >
                     <tr>
                         <th>Resources</th>
                         <th></th>
@@ -17,41 +21,61 @@ export default function Footer() {
                     </tr>
                     <tr>
                         <td>Distribute on Epik Games</td>
-                        <td>Careers</td>
-                        <td>Company</td>
-                        <td>Fan Art Policy</td>
-                        {/* <td>poqwirotijn4</td> */}
+                        <td>Server Status</td>
+                        <td>Online Services</td>
+                        <td>Battle Breakres</td>
+                        <td>Robo Recall</td>
                     </tr>
                     <tr>
-                        <td>Server Status</td>
+                        <td>Careers</td>
                         <td>UX Research</td>
                         <td>Store EULA</td>
-                        {/* <td>,c.mbptri45u</td> */}
-                        {/* <td>poqwirotijn4</td> */}
+                        <td>Fall guys</td>
+                        <td>Rocker League</td>
                     </tr>
 
                     <tr>
-                        <td>Online Services</td>
+                        <td>Company</td>
                         <td>Community Rules</td>
                         <td>Epic Newsroom</td>
-                        {/* <td>,c.mbptri45u</td>
-                        <td>poqwirotijn4</td> */}
+                        <td>Fortnite</td>
+                        <td>Shadow Complex</td>
                     </tr>
-                    
+
                     <tr>
-                        <td>kwjehroweiur</td>
+                        <td>Fan Art Policy</td>
+                        <td></td>
+                        <td></td>
+                        <td>Infinity Blade</td>
+                        <td>Unreal Tournament</td>
                     </tr>
+
 
                 </table>
             </div>
             <hr className="solid" />
             <div
-            className='discription'>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse dolorum aperiam qui illo autem, cumque, eius nulla vitae repellendus unde quos culpa officia laboriosam tempora, impedit beatae. Esse, modi ab.
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium maxime beatae id expedita vel, repellat labore quo omnis tempora est sapiente tenetur veritatis. Perspiciatis, natus reiciendis minima provident laudantium similique?
+                className='discription'>
+                © 2023, Epic Games,
+                Inc. All rights reserved. Epic, Epic Games, the Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine, the Unreal Engine logo, Unreal Tournament, and the
+                <br /> Unreal Tournament logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere. Other brands or product names are the trademarks of their respective owners.
             </div>
-
+            <div
+            className='links'>
+                <a href="">
+                    Terms of Service
+                </a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="">
+                    Private Policy
+                </a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="">Store Refund Policy</a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="">Account Security</a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="">Cookies Settings</a>
+            </div>
         </div>
     )
 }
