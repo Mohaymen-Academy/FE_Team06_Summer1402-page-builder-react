7b7feff3f49d04353235870f1a10a81102621fe5
hesam <bhesam2000@yahoo.com>
2023-07-19 11:31:57 +0430

    vitrin 90% completed

--- vitrin/src/js/script.js
+++ vitrin/src/js/script.js
@@ -1,74 +1,81 @@
 document.addEventListener("DOMContentLoaded", function () {
     //TODO 
     // to add the big icons 
-    // const cardContainer = document.getElementById('CatsContainer')
-    // function addCard() {
-    //     const Icons = [
-    //         {
-    //             title: "بازی و تفریح",
-    //             src: "./assets/images/bazi.png",
-    //         },
-    //         {
-    //             title: "پخش تلویزیون",
-    //             src: "./assets/images/pakhsh.png",
-    //         },
-    //         {
-    //             title: "انیمیشن",
-    //             src: "./assets/images/tv.png",
-    //         },
-    //         {
-    //             title: "مذهبی",
-    //             src: "./assets/images/mazhabi.png",
-    //         },
-    //         {
-    //             title: "سبک زندگی",
-    //             src: "./assets/images/sabk.png",
-    //         },
-    //         {
-    //             title: "زنگ مدرسه",
-    //             src: "./assets/images/zang.png",
-    //         },
-    //         {
-    //             title: "اخبار",
-    //             src: "./assets/images/news.png",
-    //         },
-    //         {
-    //             title: "کودکانه",
-    //             src: "./assets/images/kodak.png",
-    //         },
-    //     ]
-    //     for (let cats in Icons) {
-    //         console.log(Icons[cats])
-    //         const link = document.createElement("a");
-    //         link.href = "#";
-    //         link.className = "w-max flex px-4";
+    function addCard() {
+        const cardContainer = document.getElementById('CatsContainer')
+        const Icons = [
+            {
+                title: "بازی و تفریح",
+                src: "./assets/images/bazi.png",
+            },
+            {
+                title: "پخش تلویزیون",
+                src: "./assets/images/pakhsh.png",
+            },
+            {
+                title: "انیمیشن",
+                src: "./assets/images/tv.png",
+            },
+            {
+                title: "مذهبی",
+                src: "./assets/images/mazhabi.png",
+            },
+            {
+                title: "سبک زندگی",
+                src: "./assets/images/sabk.png",
+            },
+            {
+                title: "زنگ مدرسه",
+                src: "./assets/images/zang.png",
+            },
+            {
+                title: "اخبار",
+                src: "./assets/images/news.png",
+            },
+            {
+                title: "کودکانه",
+                src: "./assets/images/kodak.png",
+            },]
+        for (let cats in Icons) {
+            console.log(Icons[cats])
+            const link = document.createElement("a");
+            link.href = "#";
+            link.className = "w-max flex px-4";
 
-    //         const cardDiv = document.createElement("div");
+            const cardDiv = document.createElement("div");
 
-    //         const imageDiv = document.createElement("div");
-    //         const image = document.createElement("img");
-    //         image.src = Icons[cats].src;
-    //         image.alt = `${Icons[cats].title}`;
-    //         imageDiv.appendChild(image);
+            const imageDiv = document.createElement("div");
+            const image = document.createElement("img");
+            image.src = Icons[cats].src;
+            image.alt = `${Icons[cats].title}`;
+            image.classList.add('w-[125px]','h-[50px]')
+            imageDiv.appendChild(image);
 
-    //         const textDiv = document.createElement("div");
-    //         textDiv.className = "text-center";
-    //         textDiv.textContent = Icons[cats].title;
+            const textDiv = document.createElement("div");
+            textDiv.className = "text-center";
+            textDiv.textContent = Icons[cats].title;
 
-    //         // Build the structure
-    //         cardDiv.appendChild(imageDiv);
-    //         cardDiv.appendChild(textDiv);
-    //         link.appendChild(cardDiv);
+            // Build the structure
+            cardDiv.appendChild(imageDiv);
+            cardDiv.appendChild(textDiv);
+            link.appendChild(cardDiv);
 
-    //         // Add the card to the container
-    //         cardContainer.appendChild(link);
-    //     }
-    // }
-    // addCard();
+            // Add the card to the container
+            cardContainer.appendChild(link);
+        }
+    }
+    addCard();
 
     function createSlideElement(element) {
         const slide = document.createElement("div");
-        slide.classList.add("slide", "flex", "justify-center", "items-center", "w-full", "text-3xl", "px-20");
+        slide.classList.add("slide", "flex", "justify-center", "items-center", 
+        // "w-full", 
+        "pl-[2.95rem]",
+        "pr-[2.5rem]",
+        // 'py-10',
+        'h-fit',
+        'pt-[10%]',
+        );
 
         const link = document.createElement("a");
         link.setAttribute("href", "");
@@ -81,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const logoImage = document.createElement("img");
         logoImage.setAttribute("src", element.src);
-        logoImage.setAttribute("style", "width: 90px; height: 90px;");
+        logoImage.setAttribute("style", "width: 75px; height: 75px;");
         logoImage.setAttribute("alt", "Logo");
 
         const logoText = document.createElement("div");
@@ -144,11 +151,13 @@ document.addEventListener("DOMContentLoaded", function () {
     function createCarousel(elementID) {
         const element = document.getElementById(elementID)
         const carouseltag = document.createElement('div');
-        carouseltag.classList.add('flex', 'flex-row', 'justify-evenly')
+        carouseltag.classList.add('flex', 'flex-row', 'justify-between')
         const title = document.createElement('h3');
+        title.classList.add('pr-[0.5rem]')
         title.textContent = element.dataset.title
         carouseltag.appendChild(title)
         const link = document.createElement('a')
+        link.classList.add('pl-[0.5rem]')
         link.classList.add('text-blue-700')
         link.textContent = element.dataset.link ? 'مشاهده همه' : '';
         link.href = `https://vitrin.splus.ir/${element.dataset.link}`
@@ -158,7 +167,7 @@ document.addEventListener("DOMContentLoaded", function () {
         carouselgrid.classList.add('grid', 'justify-between')
         const carouselconainer = document.createElement('div')
 
-        carouselconainer.classList.add('carousel-container', 'flex', 'flex-row', "w-full", "h-48", "overflow-hidden", 'relative', "gap-[10rem]",'px-[10rem]')
+        carouselconainer.classList.add('carousel-container', 'flex', 'flex-row', "w-full", "h-36", "overflow-hidden", 'relative', "gap-[10rem]")
         const carousel = document.createElement('div')
         carousel.classList.add("carousel", "flex", "transition-transform", "ease-in")
         addSlideToCarousel(carousel)
