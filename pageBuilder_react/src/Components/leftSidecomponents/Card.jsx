0174f70400b98fbe4a28d82352370cda2c7f9729
Zahra Tabatabaee <Zahra_Tabatabaee@comp.iust.ac.ir>
2023-07-31 13:41:01 +0330

    save input card

--- pageBuilder_react/src/Components/leftSidecomponents/Card.jsx
+++ pageBuilder_react/src/Components/leftSidecomponents/Card.jsx
@@ -3,32 +3,15 @@ import "../../assets/Styles/Page1.css"
 import ColorPicker from '../../utility/ColorPicker';
 import { ElementsContext } from '../Layout';
 import DropDown from '../DropDown';
+import FileUploader from '../../utility/FileUploader';
 function CardSideBar({ setters , values }) {
     const layoutContext = useContext(ElementsContext);
     const setter = layoutContext.current.setters
-    const inputref = useRef(null);
     function handleSetting(func , value)
     {
         layoutContext.current.setters[func](value);
     }
-    const onButtonClick = () => {
-        inputref.current.click();
-      }
-    const handleChange = function (e) {
-    e.preventDefault();
-    if (e.target.files && e.target.files[0]) {
-        handleFile(e.target.files);
-    }
-    };
-    const handleDrag = function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    if (e.type === "dragenter" || e.type === "dragover") {
-        setDragActive(true);
-    } else if (e.type === "dragleave") {
-        setDragActive(false);
-    }
-    };
+ 
     console.log(layoutContext.current.setters);
     return (
         <>
@@ -80,15 +63,8 @@ function CardSideBar({ setters , values }) {
                 </div>
                 
 
-                <form onClick={onButtonClick}  className=" flex flex-col justify-center items-center border-dashed border-2 border-[#0066FF] rounded-lg mobile:h-[100px] h-[150px] p-2  mb-3 " onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-                    <input ref={inputref} type="file" className={'hidden'} multiple={true} onChange={handleChange} />
-                    <div className="flex flex-col justify-center items-center w-[50px] h-[50px] rounded-lg bg-[#dce5f1]">
-                        <img className="w-[25px] h-[25px]" src="images/plus.png" />
-                    </div>
-                    <div className="text-[#0066FF] m-4 mb-1 mobile:text-xs">افزودن عکس</div>
-                    <div className="text-xs mobile:hidden">حجم عکس تا 5 مگابایت و با فرمت PNG, JPG</div>
-                </form>
-                <ColorPicker title={'رنگ پس زمینه'}setVal = {handleSetting} func="setbgColor" />
+                <FileUploader setVal = {handleSetting} func="setimage"/>
+                <ColorPicker title={'رنگ پس زمینه'} setVal = {handleSetting} func="setbgColor" />
                 <ColorPicker title={'رنگ متن'}  setVal = {handleSetting} func="settextColor"/>
 
                 <div className="flex flex-row justify-between py-[7px]  ">
