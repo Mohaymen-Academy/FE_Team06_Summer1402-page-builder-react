503b09c331a61be72eac49a75d440dd92905c66a
Zahra Tabatabaee <Zahra_Tabatabaee@comp.iust.ac.ir>
2023-07-31 14:54:56 +0330

    button side bar functionality

--- pageBuilder_react/src/Components/Hooks/useButton.jsx
+++ pageBuilder_react/src/Components/Hooks/useButton.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
 
 export default function useButton() {
+    const [align, setalign] = useState(0);
+
     const [size, setsize] = useState(120)
     const [width, setwidth] = useState(120)
     const [padding, setpadding] = useState(10);
     const [gap, setgap] = useState(6);
-    const [radisu, setradisu] = useState(10);
+    const [radisu, setradius] = useState(10);
     const [text, settext] = useState('لورم ایپسام');
     const [textColor, settextColor] = useState('#FA0A0A');
     const [bgcolor, setbgcolor] = useState('#fff142');
     const [link, setlink] = useState(null);
+    const [textAlign, settextAlign] = useState(0);
+    const [hasIcon, sethasIcon] = useState(false);
+
 
     return {
         states: {
+            align,
             bgcolor,
             size,
             width,
@@ -22,17 +28,22 @@ export default function useButton() {
             text,
             textColor,
             link,
+            textAlign,
+            hasIcon
         },
         setters: {
+            setalign,
             setbgcolor,
             setsize,
             setwidth,
             setpadding,
             setgap,
-            setradisu,
+            setradius,
             settext,
             settextColor,
-            setlink
+            setlink,
+            settextAlign,
+            sethasIcon
         },
     };
 
