7f816a98e86641674d614aece91a99533614c417
hesam <bhesam2000@yahoo.com>
2023-08-01 12:06:38 +0430

    text fixed

--- pageBuilder_react/src/Components/Hooks/useText.jsx
+++ pageBuilder_react/src/Components/Hooks/useText.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 
 export default function useText() {
-    const [fonttype, setfonttype] = useState(null);
-    const [fontfamliy, setfontfamliy] = useState(null);
+    const [fonttype, setfonttype] = useState(1);
+    const [fontfamliy, setfontfamliy] = useState(1);
     const [title, settitle] = useState(null);
     const [text, settext] = useState("            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem quam nesciunt asperiores sed corporis perspiciatis numquam iusto harum placeat quos mollitia atque amet, alias similique adipisci voluptates. Neque, mollitia facilis.");
     const [textColor, settextColor] = useState('#121212');
     const [padding, setpadding] = useState(0);
     const [gap, setgap] = useState(6);
     const [link, setlink] = useState('https://google.com/');
-    const [fontsize, setfontsize] = useState(null);
-    const [lineHeight, setlineHeight] = useState(null);
+    const [fontsize, setfontsize] = useState(10);
+    const [lineHeight, setlineHeight] = useState(1.5);
     const [textAlign, settextAlign] = useState('end');
     const [align, setalign] = useState(0);
 
--- pageBuilder_react/src/Components/leftSidecomponents/Text.jsx
+++ pageBuilder_react/src/Components/leftSidecomponents/Text.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import "../../assets/Styles/Page1.css"
 import ColorPicker from '../../utility/ColorPicker';
 import { ElementsContext } from '../Layout';
@@ -10,77 +10,76 @@ function Text() {
     const layoutContext = useContext(ElementsContext);
     const setter = layoutContext.current.setters
     console.log("HERE ", layoutContext.current.setters)
-    function handleSetting(func , value)
-    {
+    function handleSetting(func, value) {
         layoutContext.current.setters[func](value);
     }
     return (
         <>
-                <div className="my-3">متن</div>
-                <div className="flex flex-row items-center justify-between mx-[8%] mobile:mx-0  my-5 w-[80%] h-[30px]  bg-white">
-                    <img className="max-w-[25px] max-h-[23px] " src="images/2.png" onClick={()=>handleSetting("setalign",1) } />
-                    <img className="max-w-[25px] max-h-[23px] " src="images/1.png" onClick={()=>handleSetting("setalign",2) } />
-                    <img className="max-w-[25px] max-h-[23px] " src="images/3.png" onClick={()=>handleSetting("setalign",3) } />
-                    <img className="max-w-[25px] max-h-[23px] " src="images/4.png" onClick={()=>handleSetting("setalign",4) } />
-                    <img className="max-w-[25px] max-h-[23px] " src="images/5.png" onClick={()=>handleSetting("setalign",5) } />
-                    <img className="max-w-[25px] max-h-[23px] " src="images/6.png" onClick={()=>handleSetting("setalign",6) } />
-                </div>
-                <div className="flex flex-row justify-between py-[7px]">
-                    <div className="text-[12px]  my-3"> نوع متن</div>
-                    <DropDown Default={getValue(layoutContext, 'size')} title_items={{
-                        1: 'کوچک',
-                        2: 'متوسط',
-                        3: 'بزرگ',
-                        4: 'خیل بزرگ',
-                    }} width="35%" setVal = {handleSetting} func="setfontfamliy" />
-                </div>
-                <div className="flex flex-row justify-between py-[7px]">
-                    <div className="text-[12px]  my-3">نوع عنوان </div>
-                    <DropDown Default={getValue(layoutContext, 'size')} title_items={{
-                        1: 'کوچک',
-                        2: 'متوسط',
-                        3: 'بزرگ',
-                        4: 'خیل بزرگ',
-                    }} width="35%" setVal = {handleSetting} func="settitle"/>
-                </div>
-                <div className="flex flex-col justify-between py-[7px]">
-                    <div className="text-[12px] mb-3">متن مورد نظر خود را وارد کنید</div>
-                    <textarea className="left-sidebar-input" name="Text1" cols="40" rows="4" onChange={(e) => handleSetting("settext",e.target.value)} placeholder="برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نا مفهموم از صنعت چاپ و با استفافاده از طراحان گرافیک است."></textarea>
-                </div>
-                <div className="flex flex-row items-center justify-between mx-[8%] mobile:mx-0  my-5 w-[80%] h-[30px]  bg-white">
-                    <img className="max-w-[25px] max-h-[23px] " src="images/1.png" onClick={() => handleSetting("settextAlign",1)} />
-                    <img className="max-w-[25px] max-h-[23px] " src="images/2.png" onClick={() => handleSetting("settextAlign",2)}/>
-                    <img className="max-w-[25px] max-h-[23px] " src="images/3.png" onClick={() => handleSetting("settextAlign",3)}/>
-                    <img className="max-w-[25px] max-h-[23px] " src="images/4.png" onClick={() => handleSetting("settextAlign",4)}/>
-                    <img className="max-w-[25px] max-h-[23px] " src="images/5.png" onClick={() => handleSetting("settextAlign",5)}/>
-                    <img className="max-w-[25px] max-h-[23px] " src="images/6.png" onClick={() => handleSetting("settextAlign",6)}/>
-                </div>
-                
-                <ColorPicker title={'رنگ متن'} setVal = {handleSetting} func="settextColor" />
-                
-                <div className="flex flex-row justify-between py-[7px]  ">
-                    <div className="text-[12px] my-1 ">سایز فونت</div>
-                    <input className="left-sidebar-input text-center " style={{width:"20%"}} onChange={(e) => handleSetting("setfontsize",e.target.value)} defaultValue={getValue(layoutContext,'fontsize')}></input>
-                </div>
-                <div className="flex flex-row justify-between py-[7px] my-0">
-                    <div className="text-[12px] my-1">ارتفاع خط</div>
-                    <input className="left-sidebar-input text-center " style={{width:"20%"}} onChange={(e) => handleSetting("setlineHeight",e.target.value)} defaultValue={getValue(layoutContext,'lineHeight')}></input>
+            <div className="my-3">متن</div>
+            <div className="flex flex-row items-center justify-between mx-[8%] mobile:mx-0  my-5 w-[80%] h-[30px]  bg-white">
+                <img className="max-w-[25px] max-h-[23px] " src="images/2.png" onClick={() => handleSetting("setalign", 1)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/1.png" onClick={() => handleSetting("setalign", 2)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/3.png" onClick={() => handleSetting("setalign", 3)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/4.png" onClick={() => handleSetting("setalign", 4)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/5.png" onClick={() => handleSetting("setalign", 5)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/6.png" onClick={() => handleSetting("setalign", 6)} />
+            </div>
+            <div className="flex flex-row justify-between py-[7px]">
+                <div className="text-[12px]  my-3">نوع متن </div>
+                <DropDown Default={getValue(layoutContext, 'fontfamliy')} title_items={{
+                    1: 'کوچک',
+                    2: 'متوسط',
+                    3: 'بزرگ',
+                    4: 'خیل بزرگ',
+                }} width="35%" setVal={handleSetting} func="setfontfamliy" />
+            </div>
+            <div className="flex flex-row justify-between py-[7px]">
+                <div className="text-[12px]  my-3"> نوع عنوان</div>
+                <DropDown Default={getValue(layoutContext, 'fonttype')} title_items={{
+                    1: 'کوچک',
+                    2: 'متوسط',
+                    3: 'بزرگ',
+                    4: 'خیل بزرگ',
+                }} width="35%" setVal={handleSetting} func="setfonttype" />
+            </div>
+            <div className="flex flex-col justify-between py-[7px]">
+                <div className="text-[12px] mb-3">متن مورد نظر خود را وارد کنید</div>
+                <textarea className="left-sidebar-input" name="Text1" cols="40" rows="4" onChange={(e) => handleSetting("settext", e.target.value)} placeholder="برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نا مفهموم از صنعت چاپ و با استفافاده از طراحان گرافیک است."></textarea>
+            </div>
+            <div className="flex flex-row items-center justify-between mx-[8%] mobile:mx-0  my-5 w-[80%] h-[30px]  bg-white">
+                <img className="max-w-[25px] max-h-[23px] " src="images/1.png" onClick={() => handleSetting("settextAlign", 1)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/2.png" onClick={() => handleSetting("settextAlign", 2)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/3.png" onClick={() => handleSetting("settextAlign", 3)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/4.png" onClick={() => handleSetting("settextAlign", 4)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/5.png" onClick={() => handleSetting("settextAlign", 5)} />
+                <img className="max-w-[25px] max-h-[23px] " src="images/6.png" onClick={() => handleSetting("settextAlign", 6)} />
+            </div>
 
-                </div>
-                <div className="flex flex-row justify-between py-[7px]  ">
-                    <div className="text-[12px] my-1 ">حاشیه</div>
-                    <input className="left-sidebar-input text-center " style={{width:"20%"}} onChange={(e) => handleSetting("setpadding",e.target.value)} defaultValue={getValue(layoutContext,'padding')}></input>
-                </div>
-                <div className="flex flex-row justify-between py-[7px] my-0">
-                    <div className="text-[12px] my-1">فاصله</div>
-                    <input className="left-sidebar-input text-center " style={{width:"20%"}} onChange={(e) => handleSetting("setgap",e.target.value)} defaultValue={getValue(layoutContext,'gap')}></input>
+            <ColorPicker title={'رنگ متن'} defualcolor={getValue(layoutContext, 'textColor')} setVal={handleSetting} func="settextColor" />
 
-                </div>
-                <div className="flex flex-col justify-between py-[7px]">
-                    <div className="text-[12px] mb-3">لینک متن </div>
-                    <input className="left-sidebar-input  " style={{width:"100%"}} onChange={(e) => handleSetting("setlink",e)} placeholder="لینک مورد نظر خود را وارد کنید"></input>
+            <div className="flex flex-row justify-between py-[7px]  ">
+                <div className="text-[12px] my-1 ">سایز فونت</div>
+                <input className="left-sidebar-input text-center " style={{ width: "20%" }} onChange={(e) => handleSetting("setfontsize", e.target.value)} defaultValue={getValue(layoutContext, 'fontsize')}></input>
+            </div>
+            <div className="flex flex-row justify-between py-[7px] my-0">
+                <div className="text-[12px] my-1">ارتفاع خط</div>
+                <input className="left-sidebar-input text-center " style={{ width: "20%" }} onChange={(e) => handleSetting("setlineHeight", e.target.value)} defaultValue={getValue(layoutContext, 'lineHeight')}></input>
 
-                </div>
+            </div>
+            <div className="flex flex-row justify-between py-[7px]  ">
+                <div className="text-[12px] my-1 ">حاشیه</div>
+                <input className="left-sidebar-input text-center " style={{ width: "20%" }} onChange={(e) => handleSetting("setpadding", e.target.value)} defaultValue={getValue(layoutContext, 'padding')}></input>
+            </div>
+            <div className="flex flex-row justify-between py-[7px] my-0">
+                <div className="text-[12px] my-1">فاصله</div>
+                <input className="left-sidebar-input text-center " style={{ width: "20%" }} onChange={(e) => handleSetting("setgap", e.target.value)} defaultValue={getValue(layoutContext, 'gap')}></input>
+
+            </div>
+            <div className="flex flex-col justify-between py-[7px]">
+                <div className="text-[12px] mb-3">لینک متن </div>
+                <input className="left-sidebar-input  " style={{ width: "100%" }} onChange={(e) => handleSetting("setlink", e)} placeholder="لینک مورد نظر خود را وارد کنید"></input>
+
+            </div>
         </>
     );
 }
